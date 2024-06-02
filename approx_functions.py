ba5961674c1c3c63fb1e18b02080902298aaed38
pihaf <113176707+pihaf@users.noreply.github.com>
2024-06-02 23:59:31 +0700

    Excercise - Approximate functions

---
+++ approx_functions.py
@@ -0,0 +1,49 @@
+# Function to calculate factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+def approx_sin(x, n):
+    if n <= 0:
+        print("n must be a positive integer")
+        return None
+    
+    result = 0
+    for i in range(n):
+        result += ((-1) ** i) * (x ** (2 * i + 1)) / factorial(2 * i + 1)
+    
+    return result
+
+def approx_cos(x, n):
+    if n <= 0:
+        print("n must be a positive integer")
+        return None
+    
+    result = 0
+    for i in range(n):
+        result += ((-1) ** i) * (x ** (2 * i)) / factorial(2 * i)
+    
+    return result
+
+def approx_sinh(x, n):
+    if n <= 0:
+        print("n must be a positive integer")
+        return None
+    
+    result = 0
+    for i in range(n):
+        result += (x ** (2 * i + 1)) / factorial(2 * i + 1)
+    
+    return result
+
+def approx_cosh(x, n):
+    if n <= 0:
+        print("n must be a positive integer")
+        return None
+    
+    result = 0
+    for i in range(n):
+        result += (x ** (2 * i)) / factorial(2 * i)
+    
+    return result
