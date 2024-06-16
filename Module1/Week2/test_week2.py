da4ebfdff30fb72b7b3560ab4863103706d1334b
pihaf <113176707+pihaf@users.noreply.github.com>
2024-06-17 01:39:17 +0700

    Update test file

--- Module1/Week2/test_week2.py
+++ Module1/Week2/test_week2.py
@@ -1,6 +1,5 @@
 from find_max_of_kernel import max_kernel
 from count_characters_of_words import count_characters
-from count_words_of_files import count_words_from_file
 from levenshtein_distance import levenshtein_distance
 
 def test_max_kernel():
@@ -19,11 +18,6 @@ def test_count_characters_of_word():
     input_string = 'Happiness'
     expected_result = {'H': 1 , 'a': 1 , 'e': 1 , 'i': 1 , 'n': 1 , 'p': 2 , 's': 2}
     assert count_characters(input_string) == expected_result
-
-def test_count_words_of_file():
-    file_path = 'data.txt'
-    result = count_words_from_file(file_path)
-    assert result['man'] == 6
 
 def test_levenshtein_distance():
     assert levenshtein_distance('hola', 'hello') == 3
