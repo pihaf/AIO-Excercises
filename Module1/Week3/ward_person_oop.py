346b2da62b8f9e0a9bc9df71e39545afba2c7937
pihaf <113176707+pihaf@users.noreply.github.com>
2024-06-22 00:05:12 +0700

    Excercise - Softmax and Softmax class with torch

--- Module1/Week3/ward_person_oop.py
+++ Module1/Week3/ward_person_oop.py
@@ -68,21 +68,3 @@ class Ward:
                 count = count + 1
         return average/count
     
-student1 = Student(name ="studentA", yob=2010, grade ="7")
-teacher1 = Teacher(name="teacherA", yob=1969, subject="Math")
-doctor1 = Doctor(name =" doctorA ", yob=1945, specialist="Endocrinologists")
-teacher2 = Teacher ( name ="teacherB", yob =1995 , subject ="History")
-doctor2 = Doctor(name ="doctorB", yob =1975 , specialist ="Cardiologists")
-ward1 = Ward(name ="Ward1")
-ward1.add_person(student1)
-ward1.add_person(teacher1)
-ward1.add_person(teacher2)
-ward1.add_person(doctor1)
-ward1.add_person(doctor2)
-ward1.describe()
-
-ward1.sort_age()
-ward1.describe()
-
-print(ward1.compute_average())
-assert ward1.compute_average() == 1982
